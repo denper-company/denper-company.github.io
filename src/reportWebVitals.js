f23022398999f846cad686296bc4cae660e8b9e1
vpcvdc <vpcvdc@hotmail.com>
2022-08-25 00:54:46 +0530

    update package

--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -15,13 +15,16 @@ gtag("config", process.env.REACT_APP_MEASUREMENT_ID);
 
 const reportWebVitals = (onPerfEntry) => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
-    import("web-vitals").then(({ getCLS, getFCP, getFID, getLCP, getTTFB }) => {
-      getCLS(onPerfEntry);
-      getFCP(onPerfEntry);
-      getFID(onPerfEntry);
-      getLCP(onPerfEntry);
-      getTTFB(onPerfEntry);
-    });
+    import("web-vitals").then(
+      ({ onCLS, onFCP, onFID, onINP, onLCP, onTTFB }) => {
+        onCLS(onPerfEntry);
+        onFCP(onPerfEntry);
+        onFID(onPerfEntry);
+        onINP(onPerfEntry);
+        onLCP(onPerfEntry);
+        onTTFB(onPerfEntry);
+      }
+    );
   }
 };
 
