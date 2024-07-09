54cf89bc65b1b3db2785751432feb571f7292d91
vpcvdc <vpcvdc@hotmail.com>
2024-07-10 00:07:45 +0530

    add meta robots

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -27,6 +27,10 @@ export function ErrorBoundary() {
   const error = useRouteError();
   return (
     <main className="absolute inset-0 flex flex-col items-center justify-center gap-2 text-center">
+      <meta
+        name="robots"
+        content="none, noindex, nofollow, noarchive, nositelinkssearchbox, nosnippet, notranslate, noimageindex"
+      />
       <header>
         <h1 className="text-3xl">Oops!</h1>
         <h2 className="text-2xl">Sorry, an unexpected error has occurred.</h2>
