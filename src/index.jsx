c7567a99179a082c809d30d7f12378abd0f094f7
vpcvdc <vpcvdc@hotmail.com>
2024-09-08 04:55:11 +0530

    refactor

--- src/index.jsx
+++ src/index.jsx
@@ -8,24 +8,3 @@ createRoot(document.getElementById("root")).render(
     <Router />
   </StrictMode>,
 );
-
-setTimeout(async () => {
-  try {
-    if (import.meta.env.PROD && "serviceWorker" in navigator) {
-      const { Workbox } = await import(
-        "https://storage.googleapis.com/workbox-cdn/releases/7.1.0/workbox-window.prod.mjs"
-      );
-      const wb = new Workbox("/sw.js");
-      const skipWaiting = () => {
-        wb.addEventListener("controlling", () => window.location.reload());
-        wb.messageSkipWaiting();
-      };
-      wb.addEventListener("waiting", skipWaiting);
-      document.addEventListener(
-        "visibilitychange",
-        async () => !document.hidden && wb.update(),
-      );
-      await wb.register();
-    }
-  } catch (error) {}
-}, 0);
