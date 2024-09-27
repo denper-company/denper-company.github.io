ff6894c8832a45658ffa09133f38937b56ec3a74
vpcvdc <vpcvdc@hotmail.com>
2024-09-27 23:33:32 +0530

    add map

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+import { resolve } from "node:path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
@@ -6,5 +7,11 @@ export default defineConfig({
   plugins: [react()],
   build: {
     outDir: "docs",
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        map: resolve(__dirname, "map/index.html"),
+      },
+    },
   },
 });
