2c3bc886f732b3a5d5da79a57a4f5e3e83764291
vpcvdc <vpcvdc@hotmail.com>
2024-12-06 12:45:44 +0530

    refactor

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,31 @@
 import { StrictMode } from "react";
+import { prefetchDNS, preconnect, preload, preinit } from "react-dom";
 import { createRoot } from "react-dom/client";
 import Router from "/src/router";
-import "/src/index.css";
+import styleUrl from "/src/index.css?url";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <Router />
-  </StrictMode>,
-);
+function Root() {
+  prefetchDNS("https://rsms.me/");
+  preconnect("https://rsms.me/", { crossOrigin: "anonymous" });
+  preload("https://rsms.me/inter/inter.css", {
+    crossOrigin: "anonymous",
+    as: "style",
+  });
+  preinit("https://rsms.me/inter/inter.css", {
+    crossOrigin: "anonymous",
+    as: "style",
+  });
+  preload(styleUrl, { crossOrigin: "anonymous", as: "style" });
+  preinit(styleUrl, { crossOrigin: "anonymous", as: "style" });
+  return (
+    <StrictMode>
+      <link rel="manifest" href="/app.webmanifest" />
+      <Router />
+    </StrictMode>
+  );
+}
+
+createRoot(document.getElementById("root")).render(<Root />);
 
 setTimeout(async () => {
   try {
