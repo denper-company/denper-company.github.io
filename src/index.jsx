fddc9eac712853714115af44c89ded2f5dd38f0a
vpcvdc <vpcvdc@hotmail.com>
2025-01-08 02:36:20 +0530

    refactor

--- src/index.jsx
+++ src/index.jsx
@@ -3,15 +3,18 @@ import { createRoot } from "react-dom/client";
 import Router from "/src/router";
 import "/src/index.css";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <link rel="manifest" href="/app.webmanifest" />
-    <Router />
-  </StrictMode>,
-);
+document
+  .querySelectorAll('link[media="print"]')
+  .forEach((link) => (link.media = "all"));
 
 setTimeout(async () => {
   try {
+    createRoot(document.getElementById("root")).render(
+      <StrictMode>
+        <link rel="manifest" href="/app.webmanifest" />
+        <Router />
+      </StrictMode>,
+    );
     if (import.meta.env.PROD && "serviceWorker" in navigator) {
       const { Workbox } = await import("workbox-window/Workbox.mjs");
       const wb = new Workbox("/sw.js");
