c8ffad342b356e3de22dc702b2b9e2b510a13005
vpcvdc <vpcvdc@hotmail.com>
2025-01-10 09:21:26 +0530

    add font cache

--- public/sw.js
+++ public/sw.js
@@ -3,6 +3,10 @@ importScripts(
 );
 const {
   recipes: { pageCache, staticResourceCache, imageCache },
+  routing: { registerRoute },
+  strategies: { CacheFirst },
+  cacheableResponse: { CacheableResponsePlugin },
+  expiration: { ExpirationPlugin },
 } = workbox;
 self.addEventListener("install", () => self.skipWaiting());
 self.addEventListener("activate", () =>
@@ -19,3 +23,22 @@ self.addEventListener("activate", () =>
 pageCache();
 staticResourceCache();
 imageCache();
+const cacheName = "fonts";
+const matchCallback = ({ request }) => request.destination === "font";
+const maxAgeSeconds = 30 * 24 * 60 * 60;
+const maxEntries = 60;
+registerRoute(
+  matchCallback,
+  new CacheFirst({
+    cacheName,
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+      new ExpirationPlugin({
+        maxEntries,
+        maxAgeSeconds,
+      }),
+    ],
+  }),
+);
