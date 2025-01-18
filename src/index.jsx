e44388478372d95a746a947bfd82b00851e43096
vpcvdc <vpcvdc@hotmail.com>
2025-01-19 00:57:18 +0530

    refactor

--- src/index.jsx
+++ src/index.jsx
@@ -1,26 +1,27 @@
+import { StrictMode, lazy } from "react";
+import { createRoot } from "react-dom/client";
 import cssUrl from "/src/index.css?url";
 const link = document.createElement("link");
 link.rel = "stylesheet";
 link.href = cssUrl;
 document.head.appendChild(link);
+const Router = lazy(() => import("/src/router"));
+const sw = "serviceWorker" in navigator;
 document.addEventListener("DOMContentLoaded", async () => {
   try {
-    const sw = "serviceWorker" in navigator;
     for await (const link of document.querySelectorAll('link[media="print"]')) {
       link.media = "all";
     }
-    const [{ StrictMode }, { createRoot }, { default: Router }] =
-      await Promise.all([
-        import("react"),
-        import("react-dom/client"),
-        import("/src/router"),
-      ]);
     createRoot(document.getElementById("root")).render(
       <StrictMode>
         {sw && <link rel="manifest" href="/app.webmanifest" />}
         <Router />
       </StrictMode>,
     );
+  } catch (error) {}
+});
+window.addEventListener("load", async () => {
+  try {
     if (import.meta.env.PROD && sw) {
       const { Workbox } = await import("workbox-window/Workbox.mjs");
       const wb = new Workbox("/sw.js");
