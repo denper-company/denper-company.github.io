d9f19bd2bd3a015d73c0107c34acf8b2e52f2252
vpcvdc <vpcvdc@hotmail.com>
2025-06-13 23:23:14 +0530

    refactor

--- src/components/fallback/index.jsx
+++ src/components/fallback/index.jsx
@@ -1,10 +1,9 @@
-import Spin from "src/components/spin";
+import Logo from "src/components/logo";
 
 export default function Fallback() {
   return (
     <div className="absolute inset-0 flex flex-col items-center-safe justify-center-safe gap-4">
-      <Spin className="pointer-events-none h-16 motion-safe:animate-spin" />
-      <p>Loading, please wait...</p>
+      <Logo className="pointer-events-none h-16 motion-safe:animate-pulse" />
     </div>
   );
 }
