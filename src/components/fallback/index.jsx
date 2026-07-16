fe8dcb11e03bc71f452402cabc5ec035796f37dd
Perakash Chakravarthy Velusamy <vpcvdc@hotmail.com>
2026-07-16 08:51:25 +0000

    add loading text

--- src/components/fallback/index.jsx
+++ src/components/fallback/index.jsx
@@ -4,6 +4,7 @@ export default function Fallback() {
   return (
     <div className="absolute inset-0 flex flex-col items-center-safe justify-center-safe gap-4">
       <Logo className="pointer-events-none h-16 motion-safe:animate-pulse" />
+      <p>Loading, please wait...</p>
     </div>
   );
 }
