97cf82caf3b4cdab4092f113b2a5931fbd544641
Perakash Chakravarthy Velusamy <vpcvdc@hotmail.com>
2026-07-22 17:19:00 +0000

    refactor

--- src/components/logo/index.jsx
+++ src/components/logo/index.jsx
@@ -4,13 +4,16 @@ export default function Logo(props) {
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 12 12"
       fill="none"
-      stroke="currentColor"
-      strokeWidth="1"
-      strokeLinecap="round"
-      strokeLinejoin="round"
+      stroke="none"
       {...props}
     >
-      <path d="m5,1 v10 c-5,0 -5,-5 0,-5 h2 c5,0 5,-5 0,-5 v10" />
+      <path
+        d="m5,1 v10 c-5,0 -5,-5 0,-5 h2 c5,0 5,-5 0,-5 v10"
+        stroke="currentColor"
+        strokeWidth="1"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
     </svg>
   );
 }
