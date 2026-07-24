ce69fbdf4007923e3c5121fdf028f3f7d408bbaf
Perakash Chakravarthy Velusamy <vpcvdc@hotmail.com>
2026-07-24 08:03:06 +0000

    refactor

--- src/index.jsx
+++ src/index.jsx
@@ -4,24 +4,21 @@ import Router from "#src/router";
 import "#src/index.css";
 const sw = "serviceWorker" in navigator;
 document.addEventListener("DOMContentLoaded", async () => {
-  try {
-    for await (const link of document.querySelectorAll('link[media="print"]')) {
-      link.media = "all";
-    }
-  } catch {
-  } finally {
-    createRoot(document.getElementById("root")).render(
-      <StrictMode>
-        {sw && <link rel="manifest" href="/app.webmanifest" />}
-        <Router />
-      </StrictMode>,
-    );
+  for await (const link of document.querySelectorAll('link[media="print"]')) {
+    link.media = "all";
   }
+  createRoot(document.getElementById("root")).render(
+    <StrictMode>
+      {sw && <link rel="manifest" href="/app.webmanifest" />}
+      <Router />
+    </StrictMode>,
+  );
 });
 window.addEventListener("load", async () => {
   try {
     if (import.meta.env.PROD && sw) {
-      const { Workbox } = await import("workbox-window/Workbox.mjs");
+      const { Workbox } =
+        await import("https://storage.googleapis.com/workbox-cdn/releases/7.4.1/workbox-window.prod.mjs");
       const wb = new Workbox("/sw.js");
       await wb.register();
       await wb.update();
