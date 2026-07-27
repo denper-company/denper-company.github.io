2690234f325a8a72cf45ac51610960b79cd41d71
Perakash Chakravarthy Velusamy <vpcvdc@hotmail.com>
2026-07-27 10:07:10 +0000

    refactor

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Router } from "#src/router";
-import "#src/index.css";
 const sw = "serviceWorker" in navigator;
 document.addEventListener("DOMContentLoaded", async () => {
   for await (const link of document.querySelectorAll('link[media="print"]')) {
